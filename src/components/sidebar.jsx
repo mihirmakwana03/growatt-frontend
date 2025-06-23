742b0109137946c948fb94f8554c967e97003dee
Mihir Makwana <mihirpmakwana786@gmail.com>
2025-06-24 01:08:42 +0530

    Testing Deployement

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import Logo from "../../assets/logo-nav.png";
+import Logo from "/assets/logo-nav.png";
 import "./sidebar.css";
 import {
   MdDashboard,
